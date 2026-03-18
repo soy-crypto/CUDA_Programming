1fc5b85488cf1c5b529a289cbd27b6235929faa0
peterzhu <ztj_nwu@snnu.edu.cn>
2026-03-17 21:45:30 -0700

    0317

---
+++ C1/2.cpp
@@ -0,0 +1,14 @@
+#include <thrust/execution_policy.h>
+#include <thrust/universal_vector.h>
+#include <thrust/transform.h>
+#include <cstdio>
+
+int main()
+{
+    float k = 0.5;
+    float am_temp = 20;
+    thrust::univesal_vector<float> temp{42, 24, 50};
+    auto transformation = [=] __host__ __device__ (float temp) {return temp + k * (am_temp - temp);}
+    printf("step temp[0] temp[1] temp[2]\n");
+
+}
